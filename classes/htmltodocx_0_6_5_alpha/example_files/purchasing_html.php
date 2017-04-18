086882fd2d54a52c6ed51b4fbf64d2e038330cd3
webdevrt <lnarciso88@gmail.com>
2017-04-18 16:04:12 +0530

    commit
    commit

---
+++ classes/htmltodocx_0_6_5_alpha/example_files/purchasing_html.php
@@ -0,0 +1,46 @@
+<p class="red">Insert Logo & Co. Info Here as jpeg.  Try to keep it no more than 1.5” tall to preserve the formatting – delete this text before inserting logo</p>
+<h1 style="vertical-align: middle">FIELD WORK ORDER</h1>
+<p>Date: </p>
+<table>
+ <tr>
+		<td width=300>PROPOSAL SUBMITTED TO </td>
+		<td width=300>
+			<table>
+				<tr>
+					<td>PRIMARY PHONE </td>
+					<td>ALTERNATE PHONE </td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+	<tr>
+		<td width=300>&nbsp; </td>
+		<td width=300>
+      	<table>
+			<tr>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+		</table>
+		</td>
+	</tr>
+    <tr>
+		<td width=300>ADDRESS </td>
+      	<td width=300>EMAIL </td>
+	</tr> 
+	<tr>
+		<td width=300>&nbsp;</td>
+      	<td width=300 >&nbsp;</td>
+	</tr>
+	<tr>
+		<td width=300>CITY,  STATE , AND ZIP CODE</td>
+      	<td width=300>JOB NAME AND ADDRESS (if different)</td>
+	</tr>
+	<tr>
+		<td width=300>&nbsp;</td>
+      	<td width=300>&nbsp;</td>
+	</tr>
+</table>
+<h2 style="text-decoration: underline;">DETAILS:</h2>
+<p style="vertical-align: right"><strong>Total hours:<strong></p>
+<p>Respectfully Submitted by: </p>
